16f643f9f60265f149e302259702b1e22e8fbfda
Harald Mack <harald.mack@protonmail.com>
2024-08-13 21:10:58 +0200

    fix error in husky - again

--- frontend/JSNotes/tests/index.test.js
+++ frontend/JSNotes/tests/index.test.js
@@ -1,19 +1,19 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from "vitest";
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
+describe("sum test", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
 });
 
-describe('dummy test', () => {
-	it('always passes', () => {
-		expect(true).toBeTruthy();
-	});
+describe("dummy test", () => {
+  it("always passes", () => {
+    expect(true).toBeTruthy();
+  });
 });
 
-describe('dummy test2', () => {
-	it('always passes', () => {
-		expect(true).toBeTruthy();
-	});
+describe("dummy test2", () => {
+  it("always passes", () => {
+    expect(true).toBeTruthy();
+  });
 });
